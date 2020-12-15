bd26f9034ff21575aec9d90d231171d8a7a4e3c9
LAPTOP-9QCCO3IF\Pierre <pmalard1998@gmail.com>
2020-12-15 16:58:29 +0100

    floorislava et pick item

---
+++ Assets/itempick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itempick : MonoBehaviour
+{
+ 
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Item")
+
+        { 
+            transform.position = new Vector3 (0.59f, 0.96f, 1.86f);
+
+            Destroy(GameObject.FindWithTag("Item"));
+        }
+
+    }
+}
+
