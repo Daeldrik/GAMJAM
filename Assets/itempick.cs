5df12b2df0024b6ba4c436a3ef8d4d3f63a0bf66
LAPTOP-9QCCO3IF\Pierre <pmalard1998@gmail.com>
2020-12-16 15:13:16 +0100

    modif itempick et scene

--- Assets/itempick.cs
+++ Assets/itempick.cs
@@ -4,38 +4,32 @@ using UnityEngine;
 
 public class itempick : MonoBehaviour
 {
-    Vector3 PositionDepart;
+    [SerializeField]
+    int nombreObjetsObjectif;
+
+    int nombreObjetsRamasses;
+
+    [SerializeField]
+    Transform checkpointTransform;
 
     private void Start()
     {
-        PositionDepart = transform.position;
+        //PositionDepart = transform.position;
     }
     private void OnCollisionEnter(Collision collision)
     {
 
         if (collision.gameObject.tag == "Item")
-
         {
-            transform.position = PositionDepart;
-
+            nombreObjetsRamasses += 1;
+            if(nombreObjetsRamasses == nombreObjetsObjectif)
+            {
+                //On a ramassé tous les objets qu'on voulait
+                transform.position = checkpointTransform.position;
+            }
             Destroy(GameObject.FindWithTag("Item"));
         }
 
-        if (collision.gameObject.tag == "Item2")
-
-        {
-            transform.position = PositionDepart;
-
-            Destroy(GameObject.FindWithTag("Item2"));
-        }
-
-        if (collision.gameObject.tag == "Item3")
-
-        {
-            transform.position = PositionDepart;
-
-            Destroy(GameObject.FindWithTag("Item3"));
-        }
 
     }
 }
