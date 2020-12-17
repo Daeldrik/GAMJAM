65e401c9741e85b78ef5ee091d15ec51edde8a91
LAPTOP-9QCCO3IF\Pierre <pmalard1998@gmail.com>
2020-12-17 10:58:45 +0100

    victory on collision

--- Assets/itempick.cs
+++ Assets/itempick.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class itempick : MonoBehaviour
 {
@@ -30,6 +31,10 @@ public class itempick : MonoBehaviour
             Destroy(collision.gameObject);
         }
 
+        if (collision.gameObject.tag == "Item2")
+
+
+        { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); ; }
 
     }
 }
